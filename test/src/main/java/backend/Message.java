50309e2b354d284e0a8e2d8776bdb14759346880
whitej <whitej@mit.edu>
2014-11-10 13:51:30 -0500

    Made the backend files recognizable as java files to android studio.

---
+++ test/src/main/java/backend/Message.java
@@ -0,0 +1,32 @@
+package backend;
+
+/**
+ * I think this bit of code will have to be touched by a number of people, and it will help to make sure
+ * all relevant people know when something changes here
+ * A message will be used to communicate between different portions of code, that
+ * will likely be running on different threads. You will you use the static constructors to make sure that
+ * the messages that get sent will be consistent no matter what. The code that receives
+ * the message will be responsible for deciding what to do with it An example of a message constructor and the way
+ * the code should be commented and implemented can be seen below in the example. It is hard to  predict exactly what
+ * messages will need to be passed. Additionally it is common practice to separate important parts of the message with spaces
+ * or at least thats what the instructors did on a pset they gave us once in 6.005
+ * @author Justin
+ *
+ */
+public class Message {
+
+    /**
+     * This method writes a message for the BTC connector telling it to update the resistance.
+     * The message will have the following form
+     * message = "resistance" + " " + x
+     * x = [0-9]*
+     * note the above line is just something called a regular expression. The [0-9] means any digit between 0 and 9
+     * and the * after means that it can have any number of those digits in a row
+     * @return a message with the defined format
+     */
+    public static String writeResistanceUpdateForBTC(int resistanceLevel){
+        return "resistance " + Integer.toString(resistanceLevel);
+    }
+
+
+}
---
+++ test/src/main/java/backend/ResistanceLevel.java
@@ -0,0 +1,71 @@
+package backend;
+
+package back end;
+        import back end/BlueToothCommunicator.java;
+/**
+ * This class will keep track of the resistance level
+ * @author Justin
+ *
+ *note to Justin, I think only one thread will be changing this, but it's possible you may
+ *have to synchronize on the methods in order to keep this thread safe
+ */
+public class ResistanceLevel {
+    private int resistance;
+    private BlueToothCommunicator btc;
+    /**
+     * Creator method for a resistance level
+     */
+    public ResistanceLevel(BlueToothCommunicator btc){
+        this.resistance = 0;
+        this.btc = btc;
+    }
+
+    /**
+     * Creator method for a resistance level, can be called with the
+     * argument starting_reisstance to set the starting resistance
+     */
+    public ResistanceLevel(int starting_resistance){
+        this.resistance = starting_resistance;
+        this.btc = btc;
+    }
+
+    /**
+     * Lvl E
+     * A method that tells you what the current resistance is(a note here is that there are two different values
+     * we can consider, we can say what the person is actually feeling, or what the person will feel when the mechanical
+     * system catches up. From a user feedback perspective I think it would be best to do the catch up one)
+     * @return an int reporesenting the currest resistance level
+     */
+    public int getCurrentresistance(){
+        return this.resistance;
+    }
+
+    /**
+     * Lvl E
+     * A method to increase the current resistance, this method should update the value that getCurrenresistance returns
+     * as well as send a message to the BlueToothCommunicator with the good news of the increased resistance
+     * @param increaseAmount the amount by which you want to increase the resistance
+     */
+    public void increaseresistance(int increaseAmount){
+        int new_resistance = this.resistance + increaseAmount;
+
+
+    }
+
+    /**
+     * Lvl E
+     * A method to decrease the current resistance, this method should update the value that getCurrenresistance returns
+     * as well as send a message to the BlueToothCommunicator with the good news of the decreased resistance
+     * @param decreaseAmount the amount by which you want to increase the resistance
+     */
+    public void decreaseResistance(int decreaseAmount){}
+
+    /**
+     * Lvl E
+     * This method should set the current resistance to the desired level, a
+     * lso make sure to tell the BlueToothCommunicator
+     * @param desiredLevel the level that the resistance level should be
+     */
+    public void setResistance(int desiredLevel){}
+}
+
