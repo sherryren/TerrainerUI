d5cdaea91a1011e5c117d9c76f0a2c1704326ecf
khao1201 <karendhao@gmail.com>
2014-11-11 11:53:38 -0500

    completed FrontEndCommunicator minus the stuff that I need to talk with front end team about

--- test/src/main/java/backend/FrontEndCommunicator.java
+++ test/src/main/java/backend/FrontEndCommunicator.java
@@ -3,6 +3,10 @@ package backend;
 /**
  * Created by Justin on 11/10/2014.
  */
+
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+
 /**
  * This class has the job of passing messages between the front and back ends and doing the appropriate stuff
  * with those messages
@@ -10,24 +14,80 @@ package backend;
  *
  */
 public class FrontEndCommunicator {
-
+    private BlockingQueue<String> MessageFromFront;
+    private BlockingQueue<String> MessageFromBack;
+    private String newMessageFromFront;
+    private String newMessageFromBack;
     /**
      * Lvl M
      * Constructor
      */
-    public FrontEndCommunicator(){}
+    public FrontEndCommunicator(){
+        this.MessageFromFront = new LinkedBlockingQueue<String>();
+        this.MessageFromBack = new LinkedBlockingQueue<String>();
+    }
 
     /**
      * Lvl E/M
-     * a loop that will wait for messages from the front end and do the right stuff with them.
+     * a loop that will wait for messages from the front end.
      * This will be on it's own thread
      */
-    private void receiveMessagesFromFrontLoop(){}
+    private void receiveMessageFromFrontLoop(){
+        try {
+            newMessageFromFront = MessageFromFront.take();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void receiveMessageFromBackLoop(){
+        try {
+            newMessageFromBack = MessageFromBack.take();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Lvl M
+     * This method will take in a message from the front end and perform the appropriate action
+     */
+    private void parseMessageFromFront(String message){
+        String [] args = message.split(" ");
+        //still need to build
+    }
+
+    /**
+     * Lvl M
+     * This method will take in a message from the back end and perform the appropriate action
+     */
+    private void parseMessageFromBack(String message){
+        String [] args = message.split(" ");
+        //still need to build
+    }
 
     /**
      * Lvl E
-     * A method that can be called by anyone to send a message to the front end
+     * A method that can be called by anyone to send a message from the front to the backend
      */
-    public void sendMessageToFrontEnd(){}
+    public void sendMessageFromFront(String message){
+        try {
+            MessageFromFront.put(message);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Lvl E
+     * A method that can be called by anyone to send a message from the backend to the front end
+     */
+    public void sendMessageFromBack(String message){
+        try {
+            MessageFromBack.put(message);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
 
 }
